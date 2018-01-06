ac355ea12d27ccba8ebb809b84c0bba1f4c70f39
Kasia <katarzyna-maslowska@o2.pl>
2018-01-07 00:26:04 +0100

    Home Page

--- GeoHelper/Controllers/HomeController.cs
+++ GeoHelper/Controllers/HomeController.cs
@@ -5,13 +5,42 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using GeoHelper.Models;
+using GeoHelper.Data;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Identity;
 
 namespace GeoHelper.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly GeoHelperContext _context;
+        private readonly ApplicationDbContext _contextApp;
+        private readonly ILogger _logger;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public HomeController(GeoHelperContext context, ApplicationDbContext contextApp, ILogger<ObliczeniaController> logger, UserManager<ApplicationUser> userManager)
         {
+            _context = context;
+            _contextApp = contextApp;
+            _logger = logger;
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+            String email = (await _userManager.GetUserAsync(HttpContext.User))?.Email;
+            List<UsersProjects> userProjects = (from proj in _context.UsersProjects
+                                             where proj.user == email 
+                                             select proj).ToList();
+                ViewBag.numberOfProjects = userProjects.Count;
+            }
+            else
+            {
+                ViewBag.numberOfProjects = 0;
+            }
+
+
             return View();
         }
 
